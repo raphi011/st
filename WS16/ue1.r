796687c2fc0b2ee39357a7341dce224ff043a6b6
Raphael Gruber <raphi011@gmail.com>
2016-10-11 13:33:02 +0200

    added comments to all examples but 1.9

--- WS16/ue1.r
+++ WS16/ue1.r
@@ -124,18 +124,31 @@ datf <- subset(dat, Gender==0)
 # werte der männer
 datm <- subset(dat, Gender==1)
 
+# [,2] gibt alle daten aus der zweiten spalte zurück (waist, V12)
 ecdf.mf <- ecdf(dat[,2])
 ecdf.m <- ecdf(datm[,2])
 ecdf.f <- ecdf(datf[,2])
 
+# sets graphical parameters and saves old values to old.par
+# mar = margins des plots
 old.par <- par(mar=c(5, 4.5, 4, 2) + 0.1)
+
+# do.p = selbe wie do.points=FALSE, abkürzung
 plot(ecdf.mf, do.p=F, verticals=T, xlab="Waist girth [cm]",
      ylab=expression(hat(F)[n](x)), lwd=1.5, 
      main="Empirische Verteilungsfunktion")
+
+# zeichne werte der männer
 lines(ecdf.m, do.p=F, verticals=T, col="darkblue", lwd=1.5)
+
+# zeichne werte der frauen
 lines(ecdf.f, do.p=F, verticals=T, col="red2", lwd=1.5)
+
+# zeichne die legende
 legend("bottomright", c("female","male","both"),
        lty=1, col=c("red2","darkblue","black"), lwd=1.5)
+
+# lade alte graphical parameters
 par(old.par)
 
 
@@ -143,6 +156,8 @@ par(old.par)
 #=======
 # Stem-and-leaf-Plot
 #--------------------
+
+# scale definiert in wieviele teile der stem aufgeteilt wird
 stem(datm[,1], scale=2)
 stem(datf[,1], scale=2)
 stem(datf[,1])
@@ -151,7 +166,7 @@ stem(datf[,1])
 #---------------------------------
 install.packages("aplpack")
 require(aplpack)
-stem.leaf.backback(datf[,1], datm[,1], m=2, depth=F)
+stem.leaf.backback(datf[,1], datm[,1], m=2, depths=F)
 
 
 
@@ -163,17 +178,35 @@ stem.leaf.backback(datf[,1], datm[,1], m=2, depth=F)
 
 # ue1.7
 #=======
+
+# lade die werte
 dat <- read.table("euroweight.txt", header=TRUE, skip=1)
+
+# lade die spalten der table in variablen
 attach(dat)
+
 brk <- seq(7.200, 7.760, by=0.010)
+
+# teilt den plot in ein 4x2 array auf
 par(mfrow=c(4,2))
+
 for (i in 1:8) {
   subdat <- weight[batch==i] 
+  
+  # erstellt ein histon gram der daten
+  # breaks ist die einteilung der klassen
+  # xlim = von bis auf der x achse
+  # cex.main bestimmt die größe des titels
   hist(subdat, breaks=brk, freq=FALSE, main=paste("Batch",i), 
        xlim=c(7.35,7.65), xlab="weight [g]", col="lightgrey",
        cex.main=1)
+  
+  # gauss'sche kerndichteschätzung
+  # ist eine (meist) symmetrische funktion um null, deren fläche eins ist.
   lines(density(subdat), lwd=2)
 }
+
+# setzt par optionen zurück
 par(mfrow=c(1,1))
 detach(dat)
 
@@ -204,12 +237,27 @@ datf <- subset(dat, Gender==0)
 datm <- subset(dat, Gender==1)
 
 # Boxplot
+# Quantile teilen einen datensatz in einem bestimmten verhältnis auf
+# Der median ist das 50%-quantil
+# Die Quartile im boxplot teilen den datensatz in etwa vier gleich große
+# hälften, Q1 = x1/4, Q2 = x1/2 (median), Q3 = x3/4. zwischen dem 1. und 3.
+# quartil liegen die mittleren 50% der daten
+#
+# Hinges - der untere hinge ist der median der ersten hälfte der (geordneten)
+# daten, der obere hinge der median der zweiten hälfte, sie entsprechen dem
+# 1. und 3. quartil 
+# 
+# Zuerst zeichnet man die box, also ein rechteck vom 1. zum 3. quartil, der
+# median wird durch einen strich hervorgehoben, dann werden die fences
+# bestimmt h = 1.5(Q3 - Q1) ... also das 1.5 fache höhe der box
+# LF = Q1 - h, UP = Q3 + h
 boxplot(Biacromial ~ Gender, data=dat, col="darkgrey",
         names=c("female","male"), pars=list(boxwex=0.6))
 mtext("Biacromial [cm]", side=2, line=2.5)
 mtext("Boxplots", side=3, line=1)
 
 # Violinplot
+# Ist eine kombination von boxplot und kerndichteschätzung
 require(vioplot)
 vioplot(datf$Biacromial, datm$Biacromial, col="lightgrey",
         names=c("female","male"))
@@ -229,18 +277,23 @@ kennz <- function(x, ro=4) {
 }
 
 attach(dat)
+
+# summary gibt min, Q1, Median (Q2), Mittelwert, Q3 und Max aus
 by(Biacromial, Gender, summary)
+
+# gibt tukeys five number summary aus, min, LH, median, UH, max
 by(Biacromial, Gender, fivenum)
+
+# varianz: Ist eine Kennzahl für die Charakterisierung des Streuungsverhaltens
+# einer empirisch gegebenen Verteilung
+
+# streuung: Die stichprobenstreuung ist die positive wurzel aus der varianz
+
+# MAD: Mittlere absolute Abweichung ist ein natürliches streuungsmaß, hier summiert
+# man die abstände von allen werten zum median und dividiert durch die menge der 
+# werte
 by(Biacromial, Gender, kennz, ro=3)
 detach(dat)
-
-
-
-
-
-
-
-
 
 
 # ue1.9
